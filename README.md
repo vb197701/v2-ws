fb67d6f6183446e0d4cd730b4ab9fc76b2166489
vb197701 <vb197701@gmail.com>
2024-09-19 16:00:18 +0800

    up

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# v2-ws
+搭建 Shadowsocks-rust， V2ray+ Nginx + WebSocket 和 Reality, Hysteria2 代理脚本，支持 Debian、Ubuntu、Centos，并支持甲骨文ARM平台。
+
+简单点讲，没域名的用户可以安装 Reality 和 hy2 代理，有域名的可以安装 V2ray+ Nginx + WebSocket 代理，各取所需。
+
+# 运行脚本：
+
+```
+bash <(curl -Ls https://raw.githubusercontent.com/vb197701/v2ray-wss/main/tcp-wss.sh)
+```
+或者
+```
+wget https://raw.githubusercontent.com/vb197701/v2ray-wss/main/tcp-wss.sh && bash tcp-wss.sh
+```
+
+**便宜VPS推荐：** https://hostalk.net/deals.html
+
+![image](https://github.com/yeahwu/v2ray-wss/assets/13328328/99ce2c9b-4e00-490c-8469-acb65174c912)
+
+已测试系统如下：
+
+Debian 9, 10, 11, 12
+
+Ubuntu 16.04, 18.04, 20.04, 22.04
+
+CentOS 7
+
+WSS客户端配置信息保存在：
+`cat /usr/local/etc/v2ray/client.json`
+
+Shadowsocks客户端配置信息：
+`cat /etc/shadowsocks/config.json`
+
+Reality客户端配置信息保存在：
+`cat /usr/local/etc/xray/reclient.json`
+
+Hysteria2客户端配置信息保存在：
+`cat /etc/hysteria/hyclient.json`
+
+# 卸载
+
+卸载ss-rust：
+```
+systemctl stop shadowsocks
+
+rm /usr/local/bin/ssserver
+
+rm /etc/systemd/system/shadowsocks.service
+
+systemctl daemon-reload
+```
+卸载 v2ray-wss:
+```
+systemctl stop v2ray
+
+systemctl stop nginx
+
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
+
+apt purge nginx -y
+
+systemctl daemon-reload
+```
+卸载 Reality:
+```
+systemctl stop xray
+
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ remove
+```
+卸载 Hysteria2:
+```
+bash <(curl -fsSL https://get.hy2.sh/) --remove
+```
+1024.day 帖子：
+https://1024.day/d/1296
+
+**提醒：连不上的朋友，建议先检查一下服务器自带防火墙有没有关闭？**
---
+++ tcp-wss.sh
@@ -0,0 +1,264 @@
+##!/bin/sh
+# forum: https://1024.day
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+timedatectl set-timezone Asia/Shanghai
+v2path=$(cat /dev/urandom | head -1 | md5sum | head -c 6)
+v2uuid=$(cat /proc/sys/kernel/random/uuid)
+ssport=$(shuf -i 2000-65000 -n 1)
+
+getIP(){
+    local serverIP=
+    serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    if [[ -z "${serverIP}" ]]; then
+        serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    fi
+    echo "${serverIP}"
+}
+
+install_precheck(){
+    echo "====输入已经DNS解析好的域名===="
+    read domain
+
+    read -t 15 -p "回车或等待15秒为默认端口443，或者自定义端口请输入(1-65535)："  getPort
+    if [ -z $getPort ];then
+        getPort=443
+    fi
+    
+    if [ -f "/usr/bin/apt-get" ]; then
+        apt-get update -y && apt-get upgrade -y
+        apt-get install -y net-tools curl
+    else
+        yum update -y && yum upgrade -y
+        yum install -y epel-release
+        yum install -y net-tools curl
+    fi
+
+    sleep 3
+    isPort=`netstat -ntlp| grep -E ':80 |:443 '`
+    if [ "$isPort" != "" ];then
+        clear
+        echo " ================================================== "
+        echo " 80或443端口被占用，请先释放端口再运行此脚本"
+        echo
+        echo " 端口占用信息如下："
+        echo $isPort
+        echo " ================================================== "
+        exit 1
+    fi
+}
+
+install_nginx(){
+    if [ -f "/usr/bin/apt-get" ];then
+        apt-get install -y nginx cron socat
+    else
+        yum install -y nginx cronie socat
+    fi
+
+cat >/etc/nginx/nginx.conf<<EOF
+pid /var/run/nginx.pid;
+worker_processes auto;
+worker_rlimit_nofile 51200;
+events {
+    worker_connections 1024;
+    multi_accept on;
+    use epoll;
+}
+http {
+    server_tokens off;
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 120s;
+    keepalive_requests 10000;
+    types_hash_max_size 2048;
+    include /etc/nginx/mime.types;
+    access_log off;
+    error_log /dev/null;
+
+    server {
+        listen 80;
+        listen [::]:80;
+        server_name $domain;
+        location / {
+            return 301 https://\$server_name\$request_uri;
+        }
+    }
+    
+    server {
+        listen $getPort ssl http2;
+        listen [::]:$getPort ssl http2;
+        server_name $domain;
+        ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+        ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE;
+        ssl_prefer_server_ciphers on;
+        ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;        
+        location / {
+            default_type text/plain;
+            return 200 "Hello World !";
+        }        
+        location /$v2path {
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:8080;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$http_host;
+        }
+    }
+}
+EOF
+}
+
+acme_ssl(){    
+    curl https://get.acme.sh | sh -s email=my@example.com
+    mkdir -p /etc/letsencrypt/live/$domain
+    ~/.acme.sh/acme.sh --issue -d $domain --standalone --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "~/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /etc/letsencrypt/live/$domain/fullchain.pem --key-file /etc/letsencrypt/live/$domain/privkey.pem --reloadcmd \"systemctl start nginx\""
+}
+
+install_v2ray(){    
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+    
+cat >/usr/local/etc/v2ray/config.json<<EOF
+{
+  "inbounds": [
+    {
+      "port": 8080,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$v2uuid"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+        "path": "/$v2path"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+
+    systemctl enable v2ray.service && systemctl restart v2ray.service && systemctl restart nginx.service
+    rm -f tcp-wss.sh install-release.sh
+
+cat >/usr/local/etc/v2ray/client.json<<EOF
+{
+===========配置参数=============
+协议：VMess
+地址：${domain}
+端口：${getPort}
+UUID：${v2uuid}
+加密方式：aes-128-gcm
+传输协议：ws
+路径：/${v2path}
+底层传输：tls
+注意：8080是免流端口不需要打开tls
+}
+EOF
+
+    clear
+}
+
+install_ssrust(){
+    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/ss-rust.sh && bash ss-rust.sh
+}
+
+install_reality(){
+    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/reality.sh && bash reality.sh
+}
+
+install_ws(){
+    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/ws.sh && bash ws.sh
+}
+
+install_hy2(){
+    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/hy2.sh && bash hy2.sh
+}
+
+client_v2ray(){
+    wslink=$(echo -n "{\"port\":${getPort},\"ps\":\"1024-wss\",\"tls\":\"tls\",\"id\":\"${v2uuid}\",\"aid\":0,\"v\":2,\"host\":\"${domain}\",\"type\":\"none\",\"path\":\"/${v2path}\",\"net\":\"ws\",\"add\":\"${domain}\",\"allowInsecure\":0,\"method\":\"none\",\"peer\":\"${domain}\",\"sni\":\"${domain}\"}" | base64 -w 0)
+
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========v2ray配置参数============"
+    echo "协议：VMess"
+    echo "地址：${domain}"
+    echo "端口：${getPort}"
+    echo "UUID：${v2uuid}"
+    echo "加密方式：aes-128-gcm"
+    echo "传输协议：ws"
+    echo "路径：/${v2path}"
+    echo "底层传输：tls"
+    echo "注意：8080是免流端口不需要打开tls"
+    echo "===================================="
+    echo "vmess://${wslink}"
+    echo
+}
+
+start_menu(){
+    clear
+    echo " ================================================== "
+    echo " 论坛：https://1024.day                              "
+    echo " 介绍：一键安装SS-Rust，v2ray+wss，Reality和hy2代理    "
+    echo " 系统：Ubuntu、Debian、CentOS                        "
+    echo " ================================================== "
+    echo
+    echo " 1. 安装 Shadowsocks-rust"
+    echo " 2. 安装 v2ray+ws+tls"
+    echo " 3. 安装 Reality"
+    echo " 4. 安装 v2ray+ws"
+    echo " 5. 安装 Hysteria2"
+    echo " 0. 退出脚本"
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    install_ssrust
+    ;;
+    2)
+    install_precheck
+    install_nginx
+    acme_ssl
+    install_v2ray
+    client_v2ray
+    ;;
+    3)
+    install_reality
+    ;;
+    4)
+    install_ws
+    ;;
+    5)
+    install_hy2
+    ;;
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    echo "请输入正确数字"
+    sleep 2s
+    start_menu
+    ;;
+    esac
+}
+
+start_menu
